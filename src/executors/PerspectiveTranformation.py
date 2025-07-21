a14aa71199922a40c4ff256a9673de951c97feca
melked <ce.melikedemirtas@gmail.com>
2025-07-21 15:09:28 +0300

    fix: fixed PerspectiveTransformation.py

--- src/executors/PerspectiveTranformation.py
+++ src/executors/PerspectiveTranformation.py
@@ -1,35 +1,43 @@
 import cv2
 import numpy as np
+from sdks.novavision.src.media.image import Image
+from sdks.novavision.src.base.component import Component
+from sdks.novavision.src.helper.executor import Executor
+from components.PerspectiveTransformation.src.utils.response import build_response
+from components.PerspectiveTransformation.src.models.PackageModel import PackageModel
 
-def auto_perspective(image_path, output_path="output.jpg", width=800, height=600):
 
-    img = cv2.imread(image_path)
-    if img is None:
-        raise ValueError(f"Görüntü okunamadı: {image_path}")
+class PerspectiveTransformation(Component):
+    def __init__(self, request, bootstrap):
+        super().__init__(request, bootstrap)
+        self.request.model = PackageModel(**(self.request.data))
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (5, 5), 0)
-    edges = cv2.Canny(gray, 75, 200)
+        self.output_width = self.request.get_param("OutputWidth") or 400
+        self.output_height = self.request.get_param("OutputHeight") or 600
+        self.image = self.request.get_param("inputImage")
 
-    contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
+    @staticmethod
+    def bootstrap(config: dict) -> dict:
+        return {}
 
-    screen_cnt = None
-    for c in contours:
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-        if len(approx) == 4:
-            screen_cnt = approx.reshape(4, 2)
-            break
+    def run(self):
 
-    if screen_cnt is None:
-        h, w = img.shape[:2]
-        screen_cnt = np.array([[0, 0], [w-1, 0], [w-1, h-1], [0, h-1]], dtype="float32")
+        img = Image.get_frame(img=self.image, redis_db=self.redis_db)
 
-    dst_points = np.array([[0, 0], [width-1, 0], [width-1, height-1], [0, height-1]], dtype="float32")
-    matrix = cv2.getPerspectiveTransform(np.float32(screen_cnt), dst_points)
-    warped = cv2.warpPerspective(img, matrix, (width, height))
+        pts1 = np.float32([[0, 260], [640, 260], [0, 400], [640, 400]])
+        pts2 = np.float32(
+            [[0, 0], [self.output_width, 0], [0, self.output_height], [self.output_width, self.output_height]])
 
-    cv2.imwrite(output_path, warped)
-    return warped
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
 
+        warped = cv2.warpPerspective(img.value, matrix, (self.output_width, self.output_height))
+
+        img.value = warped
+        self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
+
+        package_model = build_response(context=self)
+        return package_model
+
+
+if __name__ == "__main__":
+    Executor(sys.argv[1]).run()
