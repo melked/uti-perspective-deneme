20adc22b4bbccc2b0d07f1a6ebf200abf537a043
melked <ce.melikedemirtas@gmail.com>
2025-07-21 14:05:26 +0300

    fix: fixed response.py

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,14 +1,14 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PackageOutputs, PackageResponse, PackageExecutor, OutputImage
+from components.PerspectiveTransformatio.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PerspectiveTransformationOutputs, PerspectiveTransformationResponse, PerspectiveTransformationExecutor, OutputImage
 
 
 def build_response(context):
     outputImage = OutputImage(value=context.image)
-    Outputs = PackageOutputs(outputImage=outputImage)
-    packageResponse = PackageResponse(outputs=Outputs)
-    packageExecutor = PackageExecutor(value=packageResponse)
-    executor = ConfigExecutor(value=packageExecutor)
+    Outputs = PerspectiveTransformationOutputs(outputImage=outputImage)
+    perspectiveTransformationeResponse = PerspectiveTransformationResponse(outputs=Outputs)
+    perspectiveTransformationExecutor = PerspectiveTransformationExecutor(value=perspectiveTransformationResponse)
+    executor = ConfigExecutor(value=perspectiveTransformationExecutor)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
