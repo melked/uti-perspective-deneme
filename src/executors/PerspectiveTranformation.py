c484fab24beb94306ff6d65a6711632283aae8e5
melked <ce.melikedemirtas@gmail.com>
2025-07-30 09:46:53 +0300

    feat: PerspectiveTranformation.py updated

--- src/executors/PerspectiveTranformation.py
+++ src/executors/PerspectiveTranformation.py
@@ -6,7 +6,7 @@ import cv2
 import numpy as np
 from typing import Any, Optional, Tuple, List
 
-from tensorflow.python.ops.clustering_ops import KMeans
+from sklearn.cluster import KMeans
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../../../"))
 
