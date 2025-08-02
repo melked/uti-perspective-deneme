6d44e16c724b2c8c4796fbef70c054af9240f0ab
melked <ce.melikedemirtas@gmail.com>
2025-08-02 23:24:32 +0300

    feat: PerspectiveTransformation.py updated

--- src/executors/PerspectiveTranformation.py
+++ src/executors/PerspectiveTranformation.py
@@ -7,7 +7,7 @@ import numpy as np
 
 # Sistem yolunu güncelleyin
 # Removed the line causing NameError: name '__file__' is not defined
-# sys.path.append(os.path.join(os.path.dirname(__file__), "../../../../"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../../../"))
 
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.component import Component
